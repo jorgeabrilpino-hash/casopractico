11f43c247644b463d85cc7f995addc1c6aa4d1b1
Arianna Chávez Rodríguez <fachima.cr@gmail.com>
2026-09-14 10:05:10 -0500

    Add Repository/IUnitOfWork.cs

--- Repository/IUnitOfWork.cs
+++ Repository/IUnitOfWork.cs
@@ -1,6 +1,16 @@
 namespace CasoPractico01.Repository;
 
-public class IUnitOfWork
-{
+public interface IUnitOfWork
+{ 
+    IClienteRepository Clientes { get;  }
+    IFacturaRepository Facturas { get;  }
+    IIngredientesRepository  Ingredientes { get;  }
+    IMenuRepository  Menus { get;  }
+    IMesaRepository Mesa { get;  }
+    IPedidoRepository  Pedido { get;  }
+    IReservaRepository  Reserva { get;  }
+    ISucursalRepository  Sucursal { get;  }
+    
+    Task<int> SaveChangesAsync();
     
 }
