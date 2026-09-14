cf98f6bca614cbaca065154afc566ed59f5e0e3e
Arianna Chávez Rodríguez <fachima.cr@gmail.com>
2026-09-14 10:12:07 -0500

    uint of work I

--- obj/Debug/net10.0/CasoPractico01.AssemblyInfo.cs
+++ obj/Debug/net10.0/CasoPractico01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasoPractico01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0daef14f54757de614cf858a8fc412829141fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasoPractico01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasoPractico01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
