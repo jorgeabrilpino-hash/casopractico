e021f9adac26c661e831f41f245c3ff88216f8c3
HAPPY TEC <jorgeabril.pino@gmail.com>
2026-09-14 09:58:35 -0500

    Carpeta implemetacion

--- Repository/IGenericoRepository.cs
+++ Repository/IGenericoRepository.cs
@@ -1,6 +1,22 @@
+using System.Linq.Expressions;
+
 namespace CasoPractico01.Repository;
 
-public class IGenericoRepository
+public interface IGenericoRepository
 {
-    
+    public interface IGenerico<T> where T : class
+    {
+        Task<IEnumerable<T>> ObtenerTodos();
+
+        Task<T?> ObtenerPorId(int id);
+
+        Task<IEnumerable<T>> Buscar(
+            Expression<Func<T, bool>> condicion);
+
+        Task Agregar(T entidad);
+
+        void Actualizar(T entidad);
+
+        void Eliminar(T entidad);
+    }
 }
